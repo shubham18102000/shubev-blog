d29108c8c602a089add218d48bb4189be9967539
shubham chamoli <shubhamchamoli8218035988@gmail.com>
2025-03-26 13:54:16 +0530

    Added all project files

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+// Define a simple route
+app.get('/', (req, res) => {
+    res.send('Hello, Backend!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
