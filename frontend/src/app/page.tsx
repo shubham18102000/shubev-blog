2cc98d41a10575667d9e6c5abbe1b51f15c5ad2a
shubham chamoli <shubhamchamoli8218035988@gmail.com>
2025-03-26 13:57:27 +0530

    Fixed frontend folder issue

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="container mx-auto py-12 px-4">
+        <h2 className="text-3xl font-bold mb-6">Welcome to shubev</h2>
+        <p className="text-gray-600">
+         Coming soon!
+        </p>
+      </div>
+    </main>
+  );
+}
